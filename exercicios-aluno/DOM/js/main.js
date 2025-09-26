1f59f7fe3e6f3a8c0242bdab7baf47986b4d5fa5
gioagostineli <giovanni.a.lima@aluno.senai.br>
2025-09-25 21:53:57 -0300

    dom

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,21 @@
+// 1. Selecionando elementos por ID
+//  - Utilizando `document.getElementById()`, selecione a `<div>` que contém o livro "O Cortiço" 
+//      e armazene sua referência em uma variável
+//  - Dê saída à variável anterior no console do navegador, observe o que é exibido
+
+saidaDaDiv = document.getElementById("o-cortico");
+console.log(saidaDaDiv)
+
+
+// 2. Selecionando elementos por Tag
+//  - Utilizando `document.getElementsByTagName()`, selecione todos os elementos `<a>` 
+//         da página , armezene seu retorno em uma variável
+//  - Dê saída à variável anterior no console do navegador, observe o que é exibido
+
+saidaA = document.getElementsByTagName('<a>');
+console.log(saidaA)
+
+
+
+
+
